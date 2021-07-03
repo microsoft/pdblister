9b315cfff91391ceef96d2e63de3509383c0768b
Justin Moore <arkolbed@gmail.com>
2021-07-03 16:35:13 -0500

    Use multiprogress bars to report per-file download progress
    This is far too verbose, but far too cool. Lol

--- src/sym.rs
+++ src/sym.rs
@@ -2,7 +2,6 @@
 #![warn(clippy::all)]
 #![allow(clippy::needless_return)]
 
-use std::error::Error;
 use std::str::FromStr;
 
 extern crate futures;
@@ -10,9 +9,11 @@ extern crate indicatif;
 extern crate reqwest;
 extern crate tokio;
 
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressFinish, ProgressStyle};
 
+use anyhow::Context;
 use futures::stream::StreamExt;
+use tokio::io::AsyncWriteExt;
 
 enum DownloadStatus {
     AlreadyExists,
@@ -65,9 +66,7 @@ fn parse_servers(srvstr: String) -> anyhow::Result<Vec<SymSrv>> {
 
     server_list
         .into_iter()
-        .map(|symstr| {
-            symstr.parse::<SymSrv>()
-        })
+        .map(|symstr| symstr.parse::<SymSrv>())
         .collect()
 }
 
@@ -82,18 +81,19 @@ pub async fn download_manifest(srvstr: String, files: Vec<String>) -> anyhow::Re
     let srv = &srvs[0];
 
     // Create the directory first, if it does not exist.
-    std::fs::create_dir_all(srv.filepath.clone())?;
+    std::fs::create_dir_all(srv.filepath.clone()).context("Failed to create symbol directory")?;
 
     // http://patshaughnessy.net/2020/1/20/downloading-100000-files-using-async-rust
     // The following code is based off of the above blog post.
     let client = reqwest::Client::new();
 
-    // Create a progress bar.
-    let pb = ProgressBar::new(files.len() as u64);
+    let m = MultiProgress::new();
 
+    // Create a progress bar.
+    let pb = m.add(ProgressBar::new(files.len() as u64));
     pb.set_style(
         ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} ({eta}) {msg}")
+            .template("[{elapsed_precise}] {bar:.cyan/blue} {pos:>10}/{len:10} ({eta}) {msg}")
             .progress_chars("##-"),
     );
 
@@ -107,6 +107,7 @@ pub async fn download_manifest(srvstr: String, files: Vec<String>) -> anyhow::Re
             let client = &client;
             let srv = &srv;
             let pb = pb.clone();
+            let m = &m;
 
             async move {
                 // Break out the filename into the separate components.
@@ -115,6 +116,8 @@ pub async fn download_manifest(srvstr: String, files: Vec<String>) -> anyhow::Re
                     panic!("Invalid manifest line encountered: \"{}\"", line);
                 }
 
+                pb.inc(1);
+
                 // Create the directory tree.
                 tokio::fs::create_dir_all(format!("{}/{}/{}", srv.filepath, el[0], el[1])).await?;
 
@@ -122,16 +125,11 @@ pub async fn download_manifest(srvstr: String, files: Vec<String>) -> anyhow::Re
 
                 // Check to see if the file already exists. If so, skip it.
                 if std::path::Path::new(&format!("{}/{}", srv.filepath, pdbpath)).exists() {
-                    pb.inc(1);
                     return Ok(DownloadStatus::AlreadyExists);
                 }
 
-                // println!("{}/{}", el[0], el[1]);
-                pb.set_message(format!("{}/{}", el[1], el[0]).as_str());
-                pb.inc(1);
-
                 // Attempt to retrieve the file.
-                let req = client
+                let mut req = client
                     .get::<&str>(&format!("{}/{}", srv.server, pdbpath).to_string())
                     .send()
                     .await?;
@@ -139,17 +137,30 @@ pub async fn download_manifest(srvstr: String, files: Vec<String>) -> anyhow::Re
                     return Err(anyhow::anyhow!("File {} - Code {}", pdbpath, req.status()));
                 }
 
+                let dl_pb = m.add(ProgressBar::new(req.content_length().unwrap()));
+                dl_pb.set_style(ProgressStyle::default_bar()
+                    .template("[{elapsed_precise}] {bar:.cyan/blue} {bytes:>10}/{total_bytes:10} {msg}  ({eta})")
+                    .progress_chars("##-")
+                    .on_finish(ProgressFinish::AndClear)
+                );
+                dl_pb.set_message(format!("{}/{}", el[1], el[0]));
+
                 // Create the output file.
                 let mut file =
                     tokio::fs::File::create(format!("{}/{}", srv.filepath, pdbpath).to_string())
                         .await?;
-                tokio::io::copy(&mut req.bytes().await?.as_ref(), &mut file).await?;
+
+                // N.B: We use this in lieu of tokio::io::copy so we can update the download progress.
+                while let Some(chunk) = req.chunk().await? {
+                    dl_pb.inc(chunk.len() as u64);
+                    file.write(&chunk).await?;
+                }
 
                 return Ok(DownloadStatus::DownloadedOk);
             }
         }),
     )
-    .buffer_unordered(64)
+    .buffer_unordered(16)
     .collect::<Vec<anyhow::Result<DownloadStatus>>>();
 
     // N.B: The buffer_unordered bit above allows us to feed in 64 requests at a time to tokio.
